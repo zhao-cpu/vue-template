f9a445bc52053b7022bc1687c285b5af9a2ef52b
赵先生 <15675521581@163.com>
2024-01-29 21:22:36 +0800

    feat: @iconify/vue

---
+++ src/components/x-icon/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+
+defineOptions({ name: 'XIcon' })
+
+defineProps<{
+  icon: string
+}>()
+</script>
+
+<template>
+  <Icon :icon="icon" />
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -3,7 +3,12 @@ defineOptions({ name: 'AboutView' })
 </script>
 
 <template>
-  <div>about</div>
+  <div class="flex items-center justify-center">
+    <h2>about</h2>
+    <div class="text-28px">
+      <XIcon icon="vscode-icons:file-type-favicon" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
